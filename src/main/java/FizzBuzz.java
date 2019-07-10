6d87bf47be879b5b389d8b59fcdcc6b4aab4b4b9
Mario Grimaldi <mario.grimaldi89@gmail.com>
2019-07-10 16:39:28 +0200

    FizzBuzz number divisible by 3 = Fizz

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -1,6 +1,10 @@
 public class FizzBuzz {
     public String play(int number) {
-        if (number == 3 || number == 6 || number == 9) return "Fizz";
-        return String.valueOf(number);
+        if (isDivisibleByThree(number)) return "Fizz";
+        else return String.valueOf(number);
+    }
+
+    private boolean isDivisibleByThree(int number) {
+        return number % 3 == 0;
     }
 }
