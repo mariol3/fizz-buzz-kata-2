ed1e287ac75d27bd0489dfdb39f55f50ec2dd1c8
Mario Grimaldi <mario.grimaldi89@gmail.com>
2019-07-10 16:33:11 +0200

    Refactored play method for number non divisible by 3 and 5

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -1,7 +1,5 @@
 public class FizzBuzz {
     public String play(int number) {
-        if (number == 1) return "1";
-        else if (number == 2) return "2";
-        return "4";
+        return String.valueOf(number);
     }
 }
