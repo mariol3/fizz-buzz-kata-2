4a59896d49c4965a9ebc09795f27558bff2a961f
Mario Grimaldi <mario.grimaldi89@gmail.com>
2019-07-15 21:30:24 +0200

    BuzzBang for multiples of 5 and 7

--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -110,6 +110,11 @@ public class FizzBuzzTest {
     }
 
     @Test
+    public void thirtyFiveShouldPrintBuzzBang() {
+        assertEquals("BuzzBang", fizzBuzz.play(35));
+    }
+
+    @Test
     public void acceptance() {
         int[] numbers = new int[]           {  1,   2,    3,     4,    5,      6,      7,     8,    9,      10,    11,    12 ,   13,    14,      15 };
         String[] expected = new String[]    { "1", "2", "Fizz", "4", "Buzz", "Fizz", "Bang", "8", "Fizz", "Buzz", "11", "Fizz", "13", "Bang", "FizzBuzz" };
