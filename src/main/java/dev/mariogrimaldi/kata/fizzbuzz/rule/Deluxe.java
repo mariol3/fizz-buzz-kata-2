5826eb153e7c5e374d969762fbcd2cdc797fc39c
Mario Grimaldi <mario.grimaldi89@gmail.com>
2019-07-18 17:34:53 +0200

    Deluxe rule generalized

--- src/main/java/dev/mariogrimaldi/kata/fizzbuzz/rule/Deluxe.java
+++ src/main/java/dev/mariogrimaldi/kata/fizzbuzz/rule/Deluxe.java
@@ -1,8 +1,14 @@
 package dev.mariogrimaldi.kata.fizzbuzz.rule;
 
+import java.util.stream.Collectors;
+
 public class Deluxe implements GameRule {
     @Override
     public String applyFor(int number) {
-        return number == 11 || number == 22 || number == 33 || number == 55 || number == 77 ? "Deluxe" : "";
+        int numberOfUniqueDigits = String.valueOf(number)
+                .chars()
+                .boxed()
+                .collect(Collectors.toSet()).size();
+        return (number > 10 && numberOfUniqueDigits == 1) ? "Deluxe" : "";
     }
 }
