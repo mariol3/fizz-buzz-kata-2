e79685fef07b96e4fb99f0e3d1e90450bba85089
Mario Grimaldi <mario.grimaldi89@gmail.com>
2019-07-19 16:42:12 +0200

    Using static import of assert

--- src/test/java/dev/mariogrimaldi/kata/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/dev/mariogrimaldi/kata/fizzbuzz/FizzBuzzTest.java
@@ -8,6 +8,7 @@ import org.junit.Test;
 import java.util.Arrays;
 
 import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
 
 public class FizzBuzzTest {
 
@@ -27,132 +28,132 @@ public class FizzBuzzTest {
 
     @Test
     public void oneShouldPrintOne() {
-        Assert.assertEquals("1", fizzBuzz.play(1));
+        assertEquals("1", fizzBuzz.play(1));
     }
 
     @Test
     public void twoShouldPrintTwo() {
-        Assert.assertEquals("2", fizzBuzz.play(2));
+        assertEquals("2", fizzBuzz.play(2));
     }
 
     @Test
     public void fourShouldPrintFour() {
-        Assert.assertEquals("4", fizzBuzz.play(4));
+        assertEquals("4", fizzBuzz.play(4));
     }
 
     @Test
     public void threeShouldPrintFizz() {
-        Assert.assertEquals("Fizz", fizzBuzz.play(3));
+        assertEquals("Fizz", fizzBuzz.play(3));
     }
 
     @Test
     public void sixShouldPrintFizz() {
-        Assert.assertEquals("Fizz", fizzBuzz.play(6));
+        assertEquals("Fizz", fizzBuzz.play(6));
     }
 
     @Test
     public void nineShouldPrintFizz() {
-        Assert.assertEquals("Fizz", fizzBuzz.play(9));
+        assertEquals("Fizz", fizzBuzz.play(9));
     }
 
     @Test
     public void fiveShouldPrintBuzz() {
-        Assert.assertEquals("Buzz", fizzBuzz.play(5));
+        assertEquals("Buzz", fizzBuzz.play(5));
     }
 
     @Test
     public void tenShouldPrintBuzz() {
-        Assert.assertEquals("Buzz", fizzBuzz.play(10));
+        assertEquals("Buzz", fizzBuzz.play(10));
     }
 
     @Test
     public void twentyShouldPrintBuzz() {
-        Assert.assertEquals("Buzz", fizzBuzz.play(20));
+        assertEquals("Buzz", fizzBuzz.play(20));
     }
 
     @Test
     public void fifteenShouldPrintFizzBuzz() {
-        Assert.assertEquals("FizzBuzz", fizzBuzz.play(15));
+        assertEquals("FizzBuzz", fizzBuzz.play(15));
     }
 
     @Test
     public void thirtyShouldPrintFizzBuzz() {
-        Assert.assertEquals("FizzBuzz", fizzBuzz.play(30));
+        assertEquals("FizzBuzz", fizzBuzz.play(30));
     }
 
     @Test
     public void fourtyFiveShouldPrintFizzBuzz() {
-        Assert.assertEquals("FizzBuzz", fizzBuzz.play(45));
+        assertEquals("FizzBuzz", fizzBuzz.play(45));
     }
 
     @Test
     public void sevenShouldPrintBang() {
-        Assert.assertEquals("Bang", fizzBuzz.play(7));
+        assertEquals("Bang", fizzBuzz.play(7));
     }
 
     @Test
     public void fourteenShouldPrintBang() {
-        Assert.assertEquals("Bang", fizzBuzz.play(14));
+        assertEquals("Bang", fizzBuzz.play(14));
     }
 
     @Test
     public void twentyEightShouldPrintBang() {
-        Assert.assertEquals("Bang", fizzBuzz.play(28));
+        assertEquals("Bang", fizzBuzz.play(28));
     }
 
     @Test
     public void twentyOneShouldPrintFizzBang() {
-        Assert.assertEquals("FizzBang", fizzBuzz.play(21));
+        assertEquals("FizzBang", fizzBuzz.play(21));
     }
 
     @Test
     public void fourtyTwoShouldPrintFizzBang() {
-        Assert.assertEquals("FizzBang", fizzBuzz.play(42));
+        assertEquals("FizzBang", fizzBuzz.play(42));
     }
 
     @Test
     public void sixtyThreeShouldPrintFizzBang() {
-        Assert.assertEquals("FizzBang", fizzBuzz.play(63));
+        assertEquals("FizzBang", fizzBuzz.play(63));
     }
 
     @Test
     public void thirtyFiveShouldPrintBuzzBang() {
-        Assert.assertEquals("BuzzBang", fizzBuzz.play(35));
+        assertEquals("BuzzBang", fizzBuzz.play(35));
     }
 
     @Test
     public void oneHundredFiveShouldPrintFizzBuzzBang() {
-        Assert.assertEquals("FizzBuzzBang", fizzBuzz.play(105));
+        assertEquals("FizzBuzzBang", fizzBuzz.play(105));
     }
 
     @Test
     public void twentyTwoShouldPrintPuffDeluxe() {
-        Assert.assertEquals("PuffDeluxe", fizzBuzz.play(22));
+        assertEquals("PuffDeluxe", fizzBuzz.play(22));
     }
 
     @Test
     public void thirtyThreeShouldPrintFizzPuffDeluxe() {
-        Assert.assertEquals("FizzPuffFakeDeluxe", fizzBuzz.play(33));
+        assertEquals("FizzPuffFakeDeluxe", fizzBuzz.play(33));
     }
 
     @Test
     public void FiftyFiveShouldPrintBuzzPuffDeluxe() {
-        Assert.assertEquals("BuzzPuffFakeDeluxe", fizzBuzz.play(55));
+        assertEquals("BuzzPuffFakeDeluxe", fizzBuzz.play(55));
     }
 
     @Test
     public void SeventyFiveShouldPrintBangPuffDeluxe() {
-        Assert.assertEquals("BangPuffFakeDeluxe", fizzBuzz.play(77));
+        assertEquals("BangPuffFakeDeluxe", fizzBuzz.play(77));
     }
 
     @Test
     public void oneHundredSixtyFiveShouldPrintFizzBuzzPuff() {
-        Assert.assertEquals("FizzBuzzPuff", fizzBuzz.play(165));
+        assertEquals("FizzBuzzPuff", fizzBuzz.play(165));
     }
 
     @Test
     public void oneThousandOneHundredFiftyFiveShouldPrintFizzBuzzBangPuff() {
-        Assert.assertEquals("FizzBuzzBangPuff", fizzBuzz.play(1155));
+        assertEquals("FizzBuzzBangPuff", fizzBuzz.play(1155));
     }
 
     @Test
@@ -161,7 +162,7 @@ public class FizzBuzzTest {
         String[] expected = new String[]    { "1", "2", "Fizz", "4", "Buzz", "Fizz", "Bang", "8", "Fizz", "Buzz", "PuffFakeDeluxe", "Fizz", "13", "Bang", "FizzBuzz" };
 
         String[] actual = Arrays.stream(numbers)
-                .mapToObj(n -> fizzBuzz.play(n))
+                .mapToObj(fizzBuzz::play)
                 .toArray(String[]::new);
 
         assertArrayEquals(expected, actual);
