1ca50be55468a928d33e0d0a248147de7c538867
Mario Grimaldi <mario.grimaldi89@gmail.com>
2019-07-22 16:21:16 +0200

    Abstracted NewDeluxe behaviour

--- src/test/java/dev/mariogrimaldi/kata/fizzbuzz/BestFizzBuzzEverTest.java
+++ src/test/java/dev/mariogrimaldi/kata/fizzbuzz/BestFizzBuzzEverTest.java
@@ -1,7 +1,6 @@
 package dev.mariogrimaldi.kata.fizzbuzz;
 
-import dev.mariogrimaldi.kata.fizzbuzz.rule.newdeluxe.BuzzDeluxe;
-import dev.mariogrimaldi.kata.fizzbuzz.rule.newdeluxe.FizzDeluxe;
+import dev.mariogrimaldi.kata.fizzbuzz.rule.DivisibleByRule;
 import dev.mariogrimaldi.kata.fizzbuzz.rule.newdeluxe.NewDeluxe;
 import org.junit.Before;
 import org.junit.Test;
@@ -17,8 +16,10 @@ public class BestFizzBuzzEverTest {
     @Before
     public void setUp() throws Exception {
         fizzBuzz = new FizzBuzz(Arrays.asList(
-                new NewDeluxe(3, "FizzDeluxe"),
-                new NewDeluxe(5, "BuzzDeluxe")
+                new DivisibleByRule(3, "Fizz"),
+                new DivisibleByRule(5, "Buzz"),
+                new NewDeluxe(3, "Deluxe"),
+                new NewDeluxe(5, "Deluxe")
         ));
     }
 
@@ -32,7 +33,11 @@ public class BestFizzBuzzEverTest {
     @Test
     public void testBuzzDeluxe() {
         assertEquals("BuzzDeluxe", fizzBuzz.play(5));
-        assertEquals("BuzzDeluxe", fizzBuzz.play(15));
         assertEquals("BuzzDeluxe", fizzBuzz.play(55));
+    }
+
+    @Test
+    public void testFizzBuzzDeluxe() {
+        assertEquals("FizzBuzzDeluxe", fizzBuzz.play(15));
     }
 }
